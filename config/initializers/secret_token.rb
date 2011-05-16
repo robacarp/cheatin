31d03e5cf8dd5ebbe02ce201ec05b0835b57289b
Robert L. Carpenter <robacarp@gmail.com>
2011-05-16 13:41:58 -0600

    bare rails, with .rvmrc => 1.8.7@cheatin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheatin::Application.config.secret_token = '7f6cb17d4144e3d697ba6c9093b96f0a20e617164264620d28addd59c32955e69d8ec61d45c8d960b6b3cd02313d43ffce27878f23320e94d688028b5752dd53'
