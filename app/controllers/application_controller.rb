e27d0cd59bd73b05de4491e4e04b2a067f2b635c
Robert L. Carpenter <robacarp@gmail.com>
2011-08-28 10:35:03 -0600

    basic authorization framework

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::Base
-#  protect_from_forgery
   before_filter :sessionify
+
+  helper_method :authorize!, :current_user, :user_signed_in?
+
+#protect_from_forgery
+
+  def authorize! resource = nil
+    case
+    when resource.nil?
+      true
+    when resource.class == 'Symbol'
+      authorize_by_symbol resource
+    else
+      authorize_by_class resource
+    end
+  end
+
   def sessionify
     if session[:user_id]
       @current_user = User.find session[:user_id]
@@ -15,4 +30,24 @@ class ApplicationController < ActionController::Base
   def user_signed_in?
     !current_user.nil? && !current_user.new_record?
   end
+
+private
+  def authorize_by_symbol sym
+    case
+    when sym == :create_sheet && current_user.admin?
+      true
+    else
+      false
+    end
+  end
+
+  def authorize_by_class cls
+    case
+    when current_user.admin?
+      true
+    else
+      false
+    end
+  end
+
 end
