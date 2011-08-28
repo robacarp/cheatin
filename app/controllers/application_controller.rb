e8860c2c25b3e88f598f0a4d7900f6d96a9bc77f
Robert L. Carpenter <robacarp@gmail.com>
2011-08-28 10:33:30 -0600

    User auth methods to retrieve user from session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
 #  protect_from_forgery
+  before_filter :sessionify
+  def sessionify
+    if session[:user_id]
+      @current_user = User.find session[:user_id]
+    end
+    #TODO add ActiveRecord NotFound catching
+  end
+
+  def current_user
+    @current_user ||= sessionify || User.new
+  end
+
+  def user_signed_in?
+    !current_user.nil? && !current_user.new_record?
+  end
 end
